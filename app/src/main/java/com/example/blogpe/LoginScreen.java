52d8061e9a9e4427841f40758e349639ccdb3728
Rahul Banerjee <rahul.banerjee@phonepe.com>
2019-06-22 14:25:49 +0530

    ask permission before inserting picture

--- app/src/main/java/com/example/blogpe/LoginScreen.java
+++ app/src/main/java/com/example/blogpe/LoginScreen.java
@@ -1,10 +1,16 @@
 package com.example.blogpe;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -14,36 +20,89 @@ public class LoginScreen extends AppCompatActivity {
 
     private ImageButton pickImageButton;
     private static final int RESULT_LOAD_IMAGE = 100;
+    private static int REQUEST_CODE = 1;
+    private static int PReqCode = 1;
+    Uri pickedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        //Initialize Image
         pickImageButton = findViewById(R.id.addImage);
+
+//        pickImageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(i, RESULT_LOAD_IMAGE);
+//            }
+//        });
 
         pickImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                //Check build view for permissions
+                if(Build.VERSION.SDK_INT >= 22) {
+                    checkAndAskPermission();
+                }
+                else {
+                    openGallery();
+                }
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-            ImageButton imageView = (ImageButton) findViewById(R.id.addImage);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+//            Uri selectedImage = data.getData();
+//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+//            cursor.moveToFirst();
+//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//            String picturePath = cursor.getString(columnIndex);
+//            cursor.close();
+//            ImageButton imageView = (ImageButton) findViewById(R.id.addImage);
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//        }
+//    }
+
+    private void openGallery() {
+
+        Intent galleyIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleyIntent.setType("image/*");
+        startActivityForResult(galleyIntent, REQUEST_CODE);
+
+    }
+
+    private void checkAndAskPermission() {
+        if(ContextCompat.checkSelfPermission(LoginScreen.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(LoginScreen.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            }
+            else {
+                ActivityCompat.requestPermissions(LoginScreen.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+        }
+        else {
+            openGallery();
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE && data != null) {
+            //Upload successful
+            //Add image to Uri
+            pickedImageUri = data.getData();
+            pickImageButton.setImageURI(pickedImageUri);
+
+        }
+    }
 }
